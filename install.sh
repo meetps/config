c1461335dd50a7bf250acd661e58d1ef1b0f2360
meetshah1995 <meetshah1995@gmail.com>
2018-07-19 16:41:33 -0700

    Add vimcat

--- install.sh
+++ install.sh
@@ -70,6 +70,10 @@ vim_update() {
     # Copy color scheme
     cp $CONFIGDIR/vim/monokai.vim ~/.vim/colors/
     
+    # Vimcat binary
+    sudo cp $CONFIGDIR/vim/vimcat /usr/bin/vimcat
+    sudo chmod +x /usr/bin/vimcat
+
     # Clone and Install vundle
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     cp $CONFIGDIR/vim/vimrc ~/.vimrc
@@ -142,7 +146,7 @@ common_update() {
 
 # Call device specific functions
 if [ "$1" = "server" ] ; then
-    common_updates
+    common_update
     rmate_update
 elif [ "$1" = "laptop" ] ; then
     common_update
